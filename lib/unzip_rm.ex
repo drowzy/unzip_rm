c62fa365b65bf714e187961359c8135d23cace30
Simon Thörnqvist <simon.thornqvist@gmail.com>
2016-05-06 13:14:53 +0200

    extract_all & remove_all, print fns

--- lib/unzip_rm.ex
+++ lib/unzip_rm.ex
@@ -12,15 +12,43 @@ defmodule UnzipRm do
   end
 
   def process(opts) do
-    File.ls!(opts[:path])
-    |> UnzipRm.Ext.filter_with_ext("zip")
+    archives = File.ls!(opts[:path]) |> UnzipRm.Ext.filter_with_ext("zip")
+
+    extract_all(archives)
+    |> print_all("Extract")
+
+    delete_all(archives)
+    |> print_all("Delete")
+
+  end
+
+  defp extract_all(archives) do
+    archives
     |> Enum.map(&(UnzipRm.Ext.extract(&1)))
-    |> Enum.each(&(IO.puts("#{&1} extracted!")))
+  end
+
+  defp delete_all(archives) do
+    archives
+    |> Enum.map(&(UnzipRm.Ext.delete(&1)))
   end
 
   defp parse_args(args) do
     {opts, _, _} = OptionParser.parse(args, switches: [path: :string])
 
     opts
+  end
+
+  defp print_all(result, action) do
+    result
+    |> Enum.map(&(print_result(&1, action)))
+    |> Enum.each(&(IO.puts(&1)))
+  end
+
+  defp print_result({:ok, result}, action) do
+    "#{action}: #{result}"
+  end
+
+  defp print_result({:error, reason}, action) do
+    "#{action} failed due to #{reason}"
   end
 end
